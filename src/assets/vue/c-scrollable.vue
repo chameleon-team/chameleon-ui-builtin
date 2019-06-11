0c6cf7b3b9ac7542b02148ea5c7e4ce6f0d301ec
huyuangang <huyuangang@didichuxing.com>
2019-06-11 11:38:06 +0800

    修改scroller

---
+++ src/assets/vue/c-scrollable.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="scroller-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    height: {
+      type: Number,
+      default: -1
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
